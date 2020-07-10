dc556ffc3efa021cda686e00872800ef9d625fe9
Janis Schickram <janis.schickram@hs-augsburg.de>
2020-07-10 11:50:28 +0200

    edited latex parameter

--- bericht/conf.py
+++ bericht/conf.py
@@ -146,8 +146,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'sphinxbericht.tex', 'sphinxbericht Documentation',
-     'Team 6', 'manual'),
+    (master_doc, 'sphinxbericht.tex', 'LICSTER Projekt SoSe 2020',
+     'IDS, Webapp, SecCom', 'manual'),
 ]
 
 
